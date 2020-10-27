9d2077611d8b4d4ae414a3a2a2e39f56296795c9
efth2007 <efth2007@hotmail.com>
2020-10-27 12:58:27 +0100

    Three doors, instructions, general layout

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let doorImage1 = document.getElementById('door1')
+let doorImage2 = document.getElementById('door2')
+let doorImage3 = document.getElementById('door3')
 
 let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
 
+let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg'
+
+let spaceDoorPath ='https://content.codecademy.com/projects/chore-door/images/space.svg'
+
 doorImage1.onclick = ()=>{
-    console.log("Clicked!")
+    console.log("Clicked on door 1!")
   doorImage1.src = botDoorPath
+}
+
+doorImage2.onclick = ()=>{
+    console.log("Clicked on door 2!")
+ doorImage2.src = beachDoorPath
+}
+
+doorImage3.onclick = ()=>{
+    console.log("Clicked on door 3!")
+ doorImage3.src = spaceDoorPath
 }
